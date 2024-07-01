65ada617f0dc30543d28c8547b7f81f17b29f292
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-07-01 16:53:09 +0700

    feat: add auth

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,11 +1,51 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
 
 const authRouter = Router();
 
-// 🐨 Todo: Exercise #1
-// ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+  const { username, password, firstName, lastName } = req.body;
+  const userData = { username, password: passwordHash, firstName, lastName };
 
-// 🐨 Todo: Exercise #3
-// ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+  const collection = db.collection("users");
+  const passwordHash = await bcrypt.hash(password, 10);
+  const result = await collection.insertOne(userData);
+  return res.json({
+    message: "User has been created successfully",
+  });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const user = await db
+    .collection("users")
+    .findOne({ username: req.body.username });
+  if (!user) {
+    return res.json({
+      message: "Invalid username or password",
+    });
+  }
+
+  const password = await bcrypt.compare(req.body.password, user.password);
+  if (!password) {
+    return res.json({
+      message: "Invalid username or password",
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, firstName: user.firstName, lastName: user.lastName },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "900000",
+    }
+  );
+  return res.json({
+    message: "login succesfully",
+    token,
+  });
+});
 
 export default authRouter;
